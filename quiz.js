56fabe01aac34ab147e9379546fe365499c17c99
neyidamichel <nm68@rice.edu>
2023-12-16 23:26:12 -0500

    Saturday Update: Skelton Css Done, Java Stucture and script skeleton almost done. Workign on Fire base

--- quiz.js
+++ quiz.js
@@ -1,32 +1,38 @@
 let correctness = [];
-let answers = [1, 5, 157];
+let answers = ["option1", "option4", "option2"];
 let question = ["What is the first positive number?", "What is 2 + 3?", "What is 158-1?"];
 let options = [[1, 2, 3, 4], [1, 2, 3, 5], [156, 157, 158, 159]];
+let index = 0
 
-function setQuestion(index) {
-    let correctOption = answers[index];
+/**
+ * Sets the question, question image, and answer options for the quiz.
+ */
+function setQuestion() {
+    
+        let correctOption = answers[index];
 
-    // Collects current data from the quiz object
-    let chosenAnswer = getSelectedOption();
-
-    // Stores correctness in an array
-    if (chosenAnswer === correctOption) {
-        correctness.push(true);
-    } else {
-        correctness.push(false);
-    }
-
-    // Set question image
-    let question = document.getElementById('questionImg');
-    question.src = "imgQuestion/question" + (index + 1) + ".png";
-
-
-    // Set answer options
-    let optionElements = document.getElementsByName('option');
-    for (let i = 0; i < optionElements.length; i++) {
-        optionElements[i].value = options[index][i];
-        optionElements[i].innerHTML = options[index][i];
-    }
+        // Collects current data from the quiz object
+        let chosenAnswer = getSelectedOption();
+        console.log(chosenAnswer);
+        // Stores correctness in an array
+        console.log(correctOption);
+        console.log(chosenAnswer === correctOption);
+        if (chosenAnswer === correctOption) {
+            correctness.push(true);
+        } else {
+            correctness.push(false);
+        }
+        index+= 1;
+        
+        if(index < question.length){
+            setUp();
+        }
+        if (index === question.length) {
+            let result = document.getElementById('result');
+            result.textContent = "You got " + correctness.filter(Boolean).length + " out of " + correctness.length + " correct!";
+                
+        }
+    
 }
 
 function getSelectedOption() {
@@ -41,4 +47,30 @@ function getSelectedOption() {
     }
 
     return selectedOption;
+}
+let timesRan = 0
+function setUp(){
+    console.log(timesRan+= 1)
+    console.log(index)
+    // Set question image
+    let questionImg = document.getElementById('questionImg');
+    questionImg.src = "imgQuestion/question" + (index + 1) + ".png";
+
+    // TODO: Add code to set the question image
+
+    // Set answer options
+    for (let i = 0; i < options[0].length; i++) {
+        console.log(i)
+        document.getElementById('option' + (i + 1)).textContent = options[index][i];
+    }
+}
+
+function outputCSV(filepath){
+    
+    let csvContent = "data:text/csv;charset=utf-8,";
+    
+    rows.forEach(function(rowArray) {
+        let row = rowArray.join(",");
+        csvContent += row + "\r\n";
+    });
 }
